c6c0237586c80ca0363ddd73bcefc56db4eb0427
a-ww2-er <warhistories101@gmail.com>
2025-01-13 00:45:57 +0100

    monorepo setup

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+console.log('hi')
+import cors from 'cors'
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send('Hello from Express')
+})
+
+app.listen(8080, () => {
+  console.log('Server started on port 8080')
+})
